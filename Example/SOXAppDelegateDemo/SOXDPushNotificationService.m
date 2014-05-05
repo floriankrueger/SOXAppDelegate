c70e2f2924911de2f8180e6e2499c59c0cf9c905
Florian Krüger <florian.krueger@gutefrage.net>
2014-05-05 15:25:44 +0200

    added basic APNS behavior to demo APNS service class

--- Example/SOXAppDelegateDemo/SOXDPushNotificationService.m
+++ Example/SOXAppDelegateDemo/SOXDPushNotificationService.m
@@ -10,4 +10,41 @@
 
 @implementation SOXDPushNotificationService
 
+// Tap into the application launch sequence and register for remote
+// notifications.
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    // Register for remote notifications
+    [application registerForRemoteNotificationTypes:
+     UIRemoteNotificationTypeBadge |
+     UIRemoteNotificationTypeSound |
+     UIRemoteNotificationTypeAlert];
+    
+    // If we launched from a remote notification, we'll pass the handler
+    // to the notification handler method.
+    if (launchOptions[UIApplicationLaunchOptionsRemoteNotificationKey]) {
+        [self application:application didReceiveRemoteNotification:launchOptions[UIApplicationLaunchOptionsRemoteNotificationKey]];
+    }
+    
+    return YES;
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    // This method is called when the device successfully registers with the Apple Push Service
+    NSLog(@"PushNotificationService Token: %@", [NSString stringWithUTF8String:deviceToken.bytes]);
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    // This method is called when the device failed to register with the Apply Push Service
+    NSLog(@"PushNotificationService Error: %@", error.localizedDescription);
+}
+
+- (void) application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    // Handler for the push notifications that we receive in our application
+    NSLog(@"PushNotificationService Received: %@", userInfo);
+}
+
 @end
